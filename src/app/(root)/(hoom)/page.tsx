694d27911c1f73914c6bcb4ea99134d3ce7908f6
Bhanu <bhanusngh12345@gmail.com>
2024-01-30 23:39:05 +0530

    Basic-Routing

---
+++ src/app/(root)/(hoom)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h1-bold'>teri maa ki chut</div>
+  )
+}
+
+export default Home
---
+++ src/app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AskQuestion = () => {
+  return (
+    <div className='h1-bold'>aa gya question puchne madherchod</div>
+  )
+}
+
+export default AskQuestion
